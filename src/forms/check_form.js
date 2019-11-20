b8b997fe41cf081f440a523f6f816e8fae0027d4
konstanty-kordas <konstanty-kordas@github.com>
2019-11-20 22:49:33 +0100

    add basic form

--- src/forms/check_form.js
+++ src/forms/check_form.js
@@ -14,7 +14,10 @@ class CheckForm extends Component {
                 id: {
                     value: ''
                 },
-                attendance:{
+                id_checkout: {
+                    value: ''
+                },
+                attendance: {
                     value: ""
                 }
             }
@@ -45,40 +48,55 @@ class CheckForm extends Component {
         console.log(this.state.formControls.surname.value)
         console.log(this.state.formControls.id.value)
         console.log(this.state.formControls.attendance.value)
+        console.log(this.state.formControls.id_checkout.value)
     }
     render() {
         return (
             <form onSubmit={this.submitFormHandler}>
                 <label htmlFor="name">Imię</label>
-                <input type="text"
+                <input
+                    type="text"
                     name="name"
                     value={this.state.formControls.name.value}
                     onChange={this.changeHandler}
                 />
                 <br />
                 <label htmlFor='surname'>Nazwisko</label>
-                <input type="text"
+                <input
+                    type="text"
                     name="surname"
                     value={this.state.formControls.surname.value}
                     onChange={this.changeHandler}
                 />
                 <br />
                 <label htmlFor="id">ID</label>
-                <input type="text"
+                <input
+                    type="text"
+                    id="id"
                     name="id"
                     value={this.state.formControls.id.value}
                     onChange={this.changeHandler}
                 />
-                <br/>
+                <br />
+                <p>TO BĘDZIE GDZIEŚ INDZIEJ</p>
+                <label htmlFor="id_checkout">ID</label>
+                <input
+                    type="text"
+                    name="id_checkout"
+                    id="id_checkout"
+                    value={this.state.formControls.id_checkout.value}
+                    onChange={this.changeHandler}></input>
                 <label htmlFor="check_in">Wejście</label>
-                <input type="radio"
-                id="check_in"
+                <input
+                    type="radio"
+                    id="check_in"
                     name="attendance"
                     value={this.state.formControls.attendance.value}
                     onChange={this.changeRadioHandler}
                 />
                 <label htmlFor="check_out">Wyjście</label>
-                <input type="radio"
+                <input
+                    type="radio"
                     id="check_out"
                     name="attendance"
                     value={this.state.formControls.attendance.value}
